a523eedb05f9d3c5f09165875b24a049c55313b7
akshay <mogerakshay@gmail.com>
2022-06-28 17:38:49 +0530

    Todo project

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Todo;
+
+class TodoController extends Controller
+{
+    public function index()
+    {   
+        $todos = Todo::all();
+        return view('welcome',[
+            'todos' => $todos
+        ]);
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'title' => 'required'
+        ]);
+
+        Todo::create($attributes);
+
+        return redirect('/');
+    }
+
+    public function update(Todo $todo)
+    {
+        $todo->update(['isDone' => true]);
+
+        return redirect('/');
+    }
+
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return redirect('/');
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css">
+
+        <!-- Styles -->
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="bg-green-200 p-4">
+        <div class="lg:w-2/4 mx-auto py-8 px-6 bg-white rounded-xl" >
+            <h1 class="font-bold text-5xl text-center mb-6">Todo App</h1>
+            <div class="mb-6">
+                <form class="flex flex-col space-y-4" method="POST" action="/">
+                    @csrf
+                    <input type="text" name="title" placeholder="Task Title" class="py-3 px-4 bg-gray-200 rounded-xl">
+                    <button class="w-28 py-4 px-8 bg-green-500 text-white rounded-xl">Add</button>
+                </form>
+            </div>
+            <hr>
+
+            <div class="mt-2">
+                @foreach($todos as $todo)
+                    <div 
+                        @class([
+                        'py-4 flex items-center border-b border-gray-300 px-3',
+                        $todo->isDone ? 'bg-blue-300 rounded-xl' : ''
+                        ])
+                    >
+                        <div class="flex-1 pr-8">
+                            <h3 class="text-lg font-semibold">{{$todo->title}}</h3>   
+                            <p>Task Created on {{date('Y-m-d',strtotime($todo->created_at))}}</p>
+                        </div>
+
+                        <div class="flex space-x-3">
+                            <form method="POST" action="/{{ $todo->id}}">
+                                @csrf
+                                @method('PATCH')
+                                    <button class="py-2 px-2 bg-green-500 text-white rounded-xl">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+                                        </svg>
+                                    </button>
+                            </form>
+                            <form method="POST" action="/{{ $todo->id }}">
+                                @csrf
+                                @method('DELETE')
+                                    <button class="py-2 px-2 bg-red-500 text-white rounded-xl">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                          <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                        </svg>
+                                    </button>
+                            </form>                 
+                        </div>          
+                    </div>
+                @endforeach          
+            </div>
+        </div>
+       </body>
+</html>
